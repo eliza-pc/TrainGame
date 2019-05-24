0f627e03a42b361f31fad9627cf339592e67e938
Robson James <james.robson.junior@gmail.com>
2019-05-23 21:49:34 -0400

    modificações na controller

--- TrainGame/Controller/GameViewController.swift
+++ TrainGame/Controller/GameViewController.swift
@@ -15,7 +15,25 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //        let scene = GameScene(size: self.view.bounds.size)
+        //        scene.backgroundColor = .clear
+        //    //    self.view.sizeToFit()
+        //        scene.scaleMode = .aspectFill
+        //
+        ////        // Copy gameplay related content over to the scene
+        ////        scene.graphs = scene.graphs
+        //
+        //        // Set the scale mode to scale to fit the window
+        //        scene.scaleMode = .aspectFill
+        //        if let skView = self.view as? SKView {
+        //            skView.showsFPS = true
+        //            skView.showsNodeCount = true
+        //            skView.ignoresSiblingOrder = true
+        //            skView.presentScene(scene)
+        //        }
+        //
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        
         // including entities and graphs.
         if let scene = GKScene(fileNamed: "GameScene") {
             
@@ -23,20 +41,18 @@ class GameViewController: UIViewController {
             if let sceneNode = scene.rootNode as! GameScene? {
                 
                 // Copy gameplay related content over to the scene
-//                sceneNode.entities = scene.entities
                 sceneNode.graphs = scene.graphs
                 
                 // Set the scale mode to scale to fit the window
                 sceneNode.scaleMode = .aspectFill
-                
+                sceneNode.position = view.center
                 // Present the scene
                 if let view = self.view as! SKView? {
                     view.presentScene(sceneNode)
-                    
                     view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
+                    view.showsPhysics = false
+                    view.showsFPS = false
+                    view.showsNodeCount = false
                 }
             }
         }
